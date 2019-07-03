bfdd58389d22e3c09cd23d935419a31d80003d62
Ofir Bartal <ofirbartal100@gmail.com>
2019-07-03 21:53:20 +0300

    try some Canny edge detection, poc of local edge detection next steps are to try edge detection on local areas with respect to the skeleton.

--- main.py
+++ main.py
@@ -17,6 +17,72 @@ def create_hist_mask(shape, point, mask_size):
     return mask
 
 
+def create_kernel(source,target,kernel_size):
+    p = source - target
+    # flip y axis
+    p *= torch.from_numpy(np.array([1.0, -1.0]))
+    if np.abs(p[0])>np.abs(p[1]):
+        # normalize to x step size
+        p *= 1 / np.abs(p[0])
+    else:
+        p *= 1 / np.abs(p[1])
+    # get the edge y scatter, and plus 2 for above and under -1 2 -1
+    # total_y = int(p[1]*kernel_size+2)
+    total_y = int( np.abs(p[1])*kernel_size+1)
+    total_x = int( np.abs(p[0])*kernel_size+1)
+
+    content = np.zeros((total_y, kernel_size))
+    kernel_down = np.zeros((kernel_size,kernel_size))
+    x = 0
+    y = 0
+    if total_x>total_y:
+        while x < kernel_size:
+            # kernel[int(y)+int(total_y/2)-1, x] = -1
+            # kernel[int(y)+int(total_y/2), x] = 2
+            # kernel[int(y)+int(total_y/2)+1, x] = -1
+
+            kernel_down[int(y) + int(total_y / 2) - 1, x] = -1
+            kernel_down[int(y) + int(total_y / 2), x] = 1
+            y += p[1]
+            x += 1
+    else:
+        while y < kernel_size:
+            # kernel[int(y)+int(total_y/2)-1, x] = -1
+            # kernel[int(y)+int(total_y/2), x] = 2
+            # kernel[int(y)+int(total_y/2)+1, x] = -1
+
+            kernel_down[y,int(x) + int(total_x / 2) - 1] = -1
+            kernel_down[y,int(x) + int(total_x / 2)] = 1
+            x += p[0]
+            y += 1
+
+    kernel_up = np.zeros((kernel_size, kernel_size))
+    x = 0
+    y = 0
+    if total_x > total_y:
+        while x < kernel_size:
+            # kernel[int(y)+int(total_y/2)-1, x] = -1
+            # kernel[int(y)+int(total_y/2), x] = 2
+            # kernel[int(y)+int(total_y/2)+1, x] = -1
+
+            kernel_up[int(y) + int(total_y / 2) + 1, x] = -1
+            kernel_up[int(y) + int(total_y / 2), x] = 1
+            y += p[1]
+            x += 1
+    else:
+        while y < kernel_size:
+            # kernel[int(y)+int(total_y/2)-1, x] = -1
+            # kernel[int(y)+int(total_y/2), x] = 2
+            # kernel[int(y)+int(total_y/2)+1, x] = -1
+
+            kernel_up[y, int(x) + int(total_x / 2) + 1] = -1
+            kernel_up[y, int(x) + int(total_x / 2)] = 1
+            x += p[0]
+            y += 1
+    return np.flip(kernel_down,axis=0),np.flip(kernel_up,axis=0)
+
+
+
 # load and extract data
 
 images_path = r"D:\TAU\Research\Project\Dataset\GANeratedDataset_v3\GANeratedHands_Release\data\noObject\\"
@@ -34,11 +100,25 @@ landmarks = torch.from_numpy(landmarks).view(-1, 2)
 # plot image , landmarks and full histogram
 
 img = cv2.imread(full_image_path, cv2.IMREAD_GRAYSCALE)
-plt.subplot(222), plt.imshow(img, cmap='gray'), plt.scatter(landmarks[:, 0], landmarks[:, 1], c='red')
-plt.subplot(221), plt.hist(img.ravel(), 256, [0, 256])
-plt.show()
-plt.clf()
+# plt.subplot(222), plt.imshow(img, cmap='gray'), plt.scatter(landmarks[:, 0], landmarks[:, 1], c='red')
+# plt.subplot(221), plt.hist(img.ravel(), 256, [0, 256])
+# plt.imshow(img, cmap='gray')
+# plt.show()
+# plt.clf()
 
+# filtered = cv2.filter2D(img,cv2.CV_8U,np.array([[-1,-1,1],[-1,1,0],[1,0,0]]))
+
+
+# kernel_down,kernel_up = create_kernel(landmarks[7],landmarks[8],5)
+#
+# filtered = cv2.filter2D(img, cv2.CV_8U, kernel_down)
+# plt.imshow(filtered, cmap='gray')
+# plt.show()
+# plt.clf()
+# filtered = cv2.filter2D(img, cv2.CV_8U, kernel_up)
+# plt.imshow(filtered, cmap='gray')
+# plt.show()
+# plt.clf()
 # Smoothing without removing edges.
 # gray_filtered = cv2.bilateralFilter(img,10 , 20 , 5)
 #
@@ -57,29 +137,41 @@ plt.clf()
 # res = center[label.flatten()]
 # res2 = res.reshape((img.shape))
 # # Applying the canny filter
-# edges = cv2.Canny(res2, 10, 200)
+edges = cv2.Canny(img, 50, 200)
 # # edges_filtered = cv2.Canny(gray_filtered, 30, 40)
 #
-# plt.imshow(edges,cmap='gray')
-# plt.show()
-# plt.clf()
+plt.imshow(edges,cmap='gray')
+plt.show()
+plt.clf()
 # plt.imshow(res2,cmap='gray')
 # plt.show()
 
 
-#
-# mask_size = 20
-# all_masksed_images_combined = np.zeros(img.shape[:2],np.uint8)
+
+mask_size = 40
+k_size=5
+all_masksed_images_combined = np.zeros(img.shape[:2],np.uint8)
 # for i in range(landmarks.size(0)):
-# # for i in [0]:
-#     hist_mask = create_hist_mask(img.shape[:2], landmarks[i], mask_size)
-#     masked_img = cv2.bitwise_and(img, img, mask=hist_mask)
-#     # all_masksed_images_combined = cv2.bitwise_or(all_masksed_images_combined,masked_img)
-#     all_masksed_images_combined = cv2.bitwise_or(all_masksed_images_combined,cv2.Canny(masked_img, 100, 200))
-#     # masked_img_hist = cv2.calcHist([masked_img], [0], hist_mask, [256], [0, 256])
-#     # plt.subplot(222), plt.imshow(masked_img, cmap='gray')#, plt.scatter(landmarks[i, 0], landmarks[i, 1], c='red')
-#     # plt.subplot(221), plt.plot(masked_img_hist)
-#     # plt.show()
-#     # plt.clf()
-# plt.imshow(all_masksed_images_combined,cmap='gray')
-# plt.show()
+for i in [1,5,9,13,17]:
+# for i in [0]:
+    for j in range(3):
+        # hist_mask = create_hist_mask(img.shape[:2], landmarks[i+j], mask_size)
+        # masked_img = cv2.bitwise_and(img, img, mask=hist_mask)
+        hist_mask = create_hist_mask(img.shape[:2], landmarks[i + j], mask_size)
+        masked_img = cv2.bitwise_and(img, img, mask=hist_mask)
+        edges = cv2.Canny(img, 100, 200)
+        masked_edges = cv2.bitwise_and(edges, edges, mask=hist_mask)
+        # kernel_down, kernel_up = create_kernel(landmarks[i+j], landmarks[i+j+1], k_size)
+        # filtered_down = cv2.filter2D(masked_img, cv2.CV_8U, kernel_down)
+        # filtered_up = cv2.filter2D(masked_img, cv2.CV_8U, kernel_up)
+        # all_masksed_images_combined = cv2.bitwise_or(all_masksed_images_combined,masked_img)
+        # all_masksed_images_combined = cv2.bitwise_or(all_masksed_images_combined,filtered_down)
+        # all_masksed_images_combined = cv2.bitwise_or(all_masksed_images_combined,filtered_up)
+        all_masksed_images_combined = cv2.bitwise_or(all_masksed_images_combined,masked_edges)
+        # masked_img_hist = cv2.calcHist([masked_img], [0], hist_mask, [256], [0, 256])
+        # plt.subplot(222), plt.imshow(masked_img, cmap='gray')#, plt.scatter(landmarks[i, 0], landmarks[i, 1], c='red')
+        # plt.subplot(221), plt.plot(masked_img_hist)
+        # plt.show()
+        # plt.clf()
+plt.imshow(all_masksed_images_combined,cmap='gray')
+plt.show()
