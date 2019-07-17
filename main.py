2b78e36832bc54c804a171751554bd7fc53a751a
Ofir Bartal <ofirbartal100@gmail.com>
2019-07-17 12:30:45 +0300

    poc of local edge detection algorithm of kernel generation according to vector of direction theta

--- main.py
+++ main.py
@@ -18,68 +18,98 @@ def create_hist_mask(shape, point, mask_size):
 
 
 def create_kernel(source, target, kernel_size):
-    def set_kernel_value_at_pos(kernel, row, col, value):
-        if row < 0 or row >= kernel.shape[0]:
-            return
-        if col < 0 or col >= kernel.shape[1]:
-            return
-        kernel[row, col] = value
+    direction = target - source
+    direction *= torch.from_numpy(np.array([1.0, -1.0]))
+    direction *= 1/direction.norm()
+    sobely = torch.from_numpy(np.array([
+        [1., 0., -1.],
+        [2., 0., -2.],
+        [1., 0., -1.]
+    ]))
 
-    p = source - target
-    # flip y axis
-    p *= torch.from_numpy(np.array([1.0, -1.0]))
+    # sobely = torch.from_numpy(np.array([
+    #     [2.,1., 0., -1.,-2.],
+    #     [2.,1., 0., -1.,-2.],
+    #     [4.,2., 0., -2.,-4.],
+    #     [2.,1., 0., -1.,-2.],
+    #     [2.,1., 0., -1.,-2.],
+    # ]))
 
-    abs_x_is_bigger = True
-    if np.abs(p[0]) > np.abs(p[1]):
-        # normalize to x step size
-        p *= 1 / np.abs(p[0])
-        abs_x_is_bigger = True
-    else:
-        p *= 1 / np.abs(p[1])
-        abs_x_is_bigger = False
+    # sobely = torch.from_numpy(np.array([
+    #     [1., 2., 0., -2., -1.],
+    #     [2., 3., 0., -3., -2.],
+    #     [3., 5., 0., -5., -3.],
+    #     [2., 3., 0., -3., -2.],
+    #     [1., 2., 0., -2., -1.],
+    # ]))
 
-    mid_point_vector = p * (kernel_size / 2)
-    translation_vector = int((kernel_size) / 2) - mid_point_vector
-    kernel_down = np.zeros((kernel_size, kernel_size))
-    x = 0
-    y = 0
-    while np.abs(x) < kernel_size and np.abs(y) < kernel_size:
-        row_pos = int(y + translation_vector[1])
-        col_pos = int(x + translation_vector[0])
-        if abs_x_is_bigger:
-            set_kernel_value_at_pos(kernel_down, row_pos - 1, col_pos, 1)
-            set_kernel_value_at_pos(kernel_down, row_pos, col_pos, -1)
-            # set_kernel_value_at_pos(kernel_down, row_pos, col_pos, 0)
-            # set_kernel_value_at_pos(kernel_down, row_pos + 1, col_pos, -1)
-        else:
-            set_kernel_value_at_pos(kernel_down, row_pos, col_pos - 1, 1)
-            set_kernel_value_at_pos(kernel_down, row_pos, col_pos, -1)
-            # set_kernel_value_at_pos(kernel_down, row_pos, col_pos, 0)
-            # set_kernel_value_at_pos(kernel_down, row_pos, col_pos + 1, -1)
+    sobelx = sobely.t()
 
-        y += p[1]
-        x += p[0]
-
-    kernel_up = np.zeros((kernel_size, kernel_size))
-    x = 0
-    y = 0
-    while np.abs(x) < kernel_size and np.abs(y) < kernel_size:
-        row_pos = int(y + translation_vector[1])
-        col_pos = int(x + translation_vector[0])
-        if abs_x_is_bigger:
-            # set_kernel_value_at_pos(kernel_up, row_pos - 1, col_pos, -1)
-            # set_kernel_value_at_pos(kernel_up, row_pos, col_pos, 0)
-            set_kernel_value_at_pos(kernel_up, row_pos, col_pos, -1)
-            set_kernel_value_at_pos(kernel_up, row_pos + 1, col_pos, 1)
-        else:
-            # set_kernel_value_at_pos(kernel_up, row_pos, col_pos - 1, -1)
-            # set_kernel_value_at_pos(kernel_up, row_pos, col_pos, 0)
-            set_kernel_value_at_pos(kernel_up, row_pos, col_pos, -1)
-            set_kernel_value_at_pos(kernel_up, row_pos, col_pos + 1, 1)
-        y += p[1]
-        x += p[0]
-
-    return np.flip(kernel_down, axis=0), np.flip(kernel_up, axis=0)
+    direction_filter = sobelx * direction[0] + sobely * direction[1]
+    # direction_filter = sobelx * 1 + sobely * 0
+    return direction_filter.numpy(),direction_filter.numpy()*-1
+    # def set_kernel_value_at_pos(kernel, row, col, value):
+    #     if row < 0 or row >= kernel.shape[0]:
+    #         return
+    #     if col < 0 or col >= kernel.shape[1]:
+    #         return
+    #     kernel[row, col] = value
+    #
+    # p = source - target
+    # # flip y axis
+    # p *= torch.from_numpy(np.array([1.0, -1.0]))
+    #
+    # abs_x_is_bigger = True
+    # if np.abs(p[0]) > np.abs(p[1]):
+    #     # normalize to x step size
+    #     p *= 1 / np.abs(p[0])
+    #     abs_x_is_bigger = True
+    # else:
+    #     p *= 1 / np.abs(p[1])
+    #     abs_x_is_bigger = False
+    #
+    # mid_point_vector = p * (kernel_size / 2)
+    # translation_vector = int((kernel_size) / 2) - mid_point_vector
+    # kernel_down = np.zeros((kernel_size, kernel_size))
+    # x = 0
+    # y = 0
+    # while np.abs(x) < kernel_size and np.abs(y) < kernel_size:
+    #     row_pos = int(y + translation_vector[1])
+    #     col_pos = int(x + translation_vector[0])
+    #     if abs_x_is_bigger:
+    #         set_kernel_value_at_pos(kernel_down, row_pos - 1, col_pos, 1)
+    #         set_kernel_value_at_pos(kernel_down, row_pos, col_pos, -1)
+    #         # set_kernel_value_at_pos(kernel_down, row_pos, col_pos, 0)
+    #         # set_kernel_value_at_pos(kernel_down, row_pos + 1, col_pos, -1)
+    #     else:
+    #         set_kernel_value_at_pos(kernel_down, row_pos, col_pos - 1, 1)
+    #         set_kernel_value_at_pos(kernel_down, row_pos, col_pos, -1)
+    #         # set_kernel_value_at_pos(kernel_down, row_pos, col_pos, 0)
+    #         # set_kernel_value_at_pos(kernel_down, row_pos, col_pos + 1, -1)
+    #
+    #     y += p[1]
+    #     x += p[0]
+    #
+    # kernel_up = np.zeros((kernel_size, kernel_size))
+    # x = 0
+    # y = 0
+    # while np.abs(x) < kernel_size and np.abs(y) < kernel_size:
+    #     row_pos = int(y + translation_vector[1])
+    #     col_pos = int(x + translation_vector[0])
+    #     if abs_x_is_bigger:
+    #         # set_kernel_value_at_pos(kernel_up, row_pos - 1, col_pos, -1)
+    #         # set_kernel_value_at_pos(kernel_up, row_pos, col_pos, 0)
+    #         set_kernel_value_at_pos(kernel_up, row_pos, col_pos, -1)
+    #         set_kernel_value_at_pos(kernel_up, row_pos + 1, col_pos, 1)
+    #     else:
+    #         # set_kernel_value_at_pos(kernel_up, row_pos, col_pos - 1, -1)
+    #         # set_kernel_value_at_pos(kernel_up, row_pos, col_pos, 0)
+    #         set_kernel_value_at_pos(kernel_up, row_pos, col_pos, -1)
+    #         set_kernel_value_at_pos(kernel_up, row_pos, col_pos + 1, 1)
+    #     y += p[1]
+    #     x += p[0]
+    #
+    # return np.flip(kernel_down, axis=0), np.flip(kernel_up, axis=0)
 
 
 # load and extract data
@@ -102,7 +132,8 @@ img = cv2.imread(full_image_path, cv2.IMREAD_GRAYSCALE)
 plt.imshow(img, cmap='gray')
 for i in [1, 5, 9, 13, 17]:
     for j in range(3):
-        plt.plot(landmarks[i+j:i+j+2, 0].numpy().squeeze(), landmarks[i+j:i+j+2, 1].numpy().squeeze(), 'ro-')
+        plt.plot(landmarks[i + j:i + j + 2, 0].numpy().squeeze(), landmarks[i + j:i + j + 2, 1].numpy().squeeze(),
+                 'ro-')
 # plt.subplot(221), plt.hist(img.ravel(), 256, [0, 256])
 # plt.imshow(img, cmap='gray')
 plt.show()
@@ -162,7 +193,12 @@ for i in [1, 5, 9, 13, 17]:
         masked_img = cv2.bitwise_and(img, img, mask=hist_mask)
         # edges = cv2.Canny(img, 100, 200)
         kernel_down, kernel_up = create_kernel(landmarks[i + j], landmarks[i + j + 1], k_size)
-        filtered_down = cv2.filter2D(masked_img, cv2.CV_8U, kernel_down)
+        # kernel_direction = create_kernel(landmarks[i + j], landmarks[i + j + 1], k_size)
+        filtered_down = cv2.filter2D(img, cv2.CV_8U, kernel_down)
+        filtered_up = cv2.filter2D(img, cv2.CV_8U, kernel_up)
+        filtered_down = cv2.bitwise_and(filtered_down, filtered_down, mask=hist_mask)
+        filtered_up = cv2.bitwise_and(filtered_up, filtered_up, mask=hist_mask)
+        # filtered_down = cv2.filter2D(masked_img, cv2.CV_8U, kernel_down)
 
         # masked_edges = cv2.bitwise_and(edges, edges, mask=hist_mask)
         # kernel_down, kernel_up = create_kernel(landmarks[i+j], landmarks[i+j+1], k_size)
@@ -173,13 +209,15 @@ for i in [1, 5, 9, 13, 17]:
         # all_masksed_images_combined = cv2.bitwise_or(all_masksed_images_combined,filtered_up)
         # all_masksed_images_combined = cv2.bitwise_or(all_masksed_images_combined,masked_edges)
         all_masksed_images_combined = cv2.bitwise_or(all_masksed_images_combined, filtered_down)
+        all_masksed_images_combined = cv2.bitwise_or(all_masksed_images_combined, filtered_up)
         # masked_img_hist = cv2.calcHist([masked_img], [0], hist_mask, [256], [0, 256])
         # plt.subplot(222), plt.imshow(masked_img, cmap='gray')#, plt.scatter(landmarks[i, 0], landmarks[i, 1], c='red')
         # plt.subplot(221), plt.plot(masked_img_hist)
         # plt.show()
         # plt.clf()
 plt.imshow(all_masksed_images_combined, cmap='gray')
-for i in [1, 5, 9, 13, 17]:
-    for j in range(3):
-        plt.plot(landmarks[i+j:i+j+2, 0].numpy().squeeze(), landmarks[i+j:i+j+2, 1].numpy().squeeze(), 'ro-')
+# for i in [1, 5, 9, 13, 17]:
+#     for j in range(3):
+#         plt.plot(landmarks[i + j:i + j + 2, 0].numpy().squeeze(), landmarks[i + j:i + j + 2, 1].numpy().squeeze(),
+#                  'ro-')
 plt.show()
